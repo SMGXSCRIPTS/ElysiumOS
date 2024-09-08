ac187b8da4227b93afaf045137ec828270097f16
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-09-08 12:55:58 -0700

    Create gdt.asm

---
+++ boot/legacy/gdt.asm
@@ -0,0 +1,30 @@
+[bits 16]
+gdt_start:
+    dd 0x00000000
+    dd 0x00000000
+
+gdt_code:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xCF
+    db 0x00
+
+gdt_data:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xCF
+    db 0x00
+
+gdt_end:
+    ;teehee
+
+gdt_desc:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
