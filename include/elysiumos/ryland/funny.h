b274463ae461c071f99bb5ae96ec6b28c566563b
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-09-08 12:14:35 -0700

    Update funny.h

--- include/elysiumos/ryland/funny.h
+++ include/elysiumos/ryland/funny.h
@@ -1,8 +1,10 @@
 #pragma once
 #ifndef __ELYSIUMOS_NONESSENTIAL_SYSTEM_PARAMETERS_H
-#define __ELYSIUMOS_NONESSENTIAL_SYSTEM_PARAMETERS_H
+#define __ELYSIUMOS_NONESSENTIAL_SYSTEM_PARAMETERS_H 1
 
 // What a bad boi...
+// Does not deserve head pats...
+// Bad driver
 void rylandDriverEntry(void);
 
 #endif//__ELYSIUMOS_NONESSENTIAL_SYSTEM_PARAMETERS_H
