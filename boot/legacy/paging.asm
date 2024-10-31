078f8233021b14b8e5465675b80bb85efdb49dba
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-10-30 17:19:46 -0700

    Create paging.asm

---
+++ boot/legacy/paging.asm
@@ -0,0 +1,31 @@
+[bits 32]
+
+PAGE_TABLE_ENTRIES equ 0x1000
+
+setupPaging:
+    pusha
+    mov edi, PAGE_TABLE_ENTRIES
+    mov cr3, edi
+
+    mov dword [edi], 0x2003
+    add edi, 0x1000
+    mov dword [edi], 0x3003
+    add edi, 0x1000
+    mov dword [edi], 0x4003
+    add edi, 0x1000
+
+    mov ebx, 0x00000003
+    mov ecx, 0x00000200
+
+setupPagingLoop:
+    cmp ecx, 0x00000000
+    je setupPagingDone
+    sub ecx, 0x00000001
+    mov dword [edi], ebx
+    add ebx, 0x00001000
+    add edi, 0x00000008
+    jmp setupPagingLoop
+
+setupPagingDone:
+    popa
+    ret
