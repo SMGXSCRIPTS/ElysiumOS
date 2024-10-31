e28af6fa071ae69768f69534d3bcf7f99dcea2ec
SMGXGAMER842 <141907473+SMGXSCRIPTS@users.noreply.github.com>
2024-10-30 17:20:25 -0700

    Create switch32.asm

---
+++ boot/legacy/switch32.asm
@@ -0,0 +1,22 @@
+[bits 16]
+switch32:
+    cli
+    lgdt [gdtDesc]
+    mov eax, cr0
+    or  eax, 0x00000001
+    mov cr0, eax
+    jmp CODE_SEG:initPM
+
+[bits 32]
+initPM:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov fs, ax
+    mov gs, ax
+
+    mov ebp, 0x7C00
+    mov esp, ebp
+
+    jmp switch64
